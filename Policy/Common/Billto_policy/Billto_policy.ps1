4ca6198377f0bc66ba849c0da05881338ebddd13
Cobey Errett <cobey.errett@gmail.com>
2018-01-18 11:59:54 -0500

    updated billto policy

---
+++ Policy/Common/Billto_policy/Billto_policy.ps1
@@ -0,0 +1,31 @@
+# To create and assign a tag-matching policy definition on a subscription
+# This will enforce a 4 numbers as a tag to the billto Tag
+
+
+$policydefname = "Matching Billto Tag"
+$policydisplayname = "Matching Tag"
+$description = "This policy enforces a 4-digit project code to be added as a tag value for the Billto tag key"
+## The pattern matching sequence is located in this JSON
+$policyURI = "matchingtagrule.json"
+
+
+$assignmentname = "Project Code Tag Matching"
+
+
+# This will apply the policy to the first subscription retrieved by the cmdlet
+$AzureSub = Get-AzureRmSubscription
+
+
+
+foreach ($sub in $AzureSub)
+{
+    Select-AzureRmSubscription -SubscriptionObject $sub
+    
+    $subscope = "/subscriptions/" + $sub.Id
+    
+    $definition = New-AzureRmPolicyDefinition -Name $policydefname -DisplayName $policydisplayname -description $description -Policy $policyURI -Mode All 
+    $definition
+    $assignment = New-AzureRMPolicyAssignment -Name $assignmentname -Scope $subscope -PolicyDefinition $definition 
+    $assignment
+    
+}
--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -24,3 +24,4 @@ resource "azurerm_virtual_network" "MyResource" {
    address_space = ["10.0.0.0/16"]
    dns_servers = ["10.0.0.4"]
 }
+
