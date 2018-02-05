caedb5a0929823e1734e4b05befff929be5b5bec
Errett <errecob@stc.ca>
2018-02-05 13:59:56 -0500

    resourcegroup creation script updated with policies

--- PowerShell/ResourceGroup/create_project_rg.ps1
+++ PowerShell/ResourceGroup/create_project_rg.ps1
@@ -1,4 +1,7 @@
-Param(
+
+function create-resourceGroup
+{
+    Param(
     [Parameter(Mandatory=$true)]   
     [string]$subscription,
     [Parameter(Mandatory=$true)]   
@@ -6,97 +9,72 @@ Param(
     [Parameter(Mandatory=$true)]   
     [string]$projectOwner,
     [Parameter(Mandatory=$true)]   
-    [string]$ad_group,
-    [Parameter(Mandatory=$true)]   
     [string]$pe,
     [string]$location="canadaeast"
-  
-)
-
-#Defines the Resource Group Name 
-$rg_name = "$projectName-$subscription-rg"
-
-function Check-ResourceGroup
-{        
-        [CmdletBinding()]
-        [OutputType([bool])]
-    Param
-    (
-        [Parameter(Mandatory=$true)]
-        [string]
-        $ResourceGroupName,
-        [string]
-        $Location,
-        [bool]
-        $add = $false
     )
-    $ResourceGroups = Get-AzureRmResourceGroup 
+    
+    #Defines the Resource Group Name 
+    $num = 1
+    
 
-    $MatchRgLocation = $ResourceGroups | where { $_.Location -like $Location} | 
-                        where {$_.ResourceGroupName -like $ResourceGroupName} 
-    if ($MatchRgLocation.Count -eq 0) { 
+    try {
+        $sub = Get-AzureRmSubscription -SubscriptionName $subscription -ErrorAction Stop   
+        Set-AzureRmContext -Subscription $sub -ErrorAction Stop
+    }
+    catch {
+        Write-Host "No Such Subscription found" -ForegroundColor Yellow
+        exit
+    }
+    
+    $created = $false    
 
-            $MatchRgLocation = $ResourceGroups | 
-                        where {$_.ResourceGroupName -like $ResourceGroupName} 
-        if ($MatchRgLocation.Count -eq 0)  {
-            Write-Verbose "$ResourceGroupName does not exist" 
-            return $false
-
-        } else {
-            Write-Verbose "$ResourceGroupName exists in alternative location" 
-            return $false
+    while (!$created) {
+        $rg_name = "$projectName$num-$subscription-rg"
+        if (!(Check-ResourceGroup -ResourceGroupName $rg_name -Location $location)) {
+            Write-Host -ForegroundColor Yellow "The RG does not exist.  Creating"
+            #creates new resource group 
+            $rg = New-AzureRmResourceGroup -Name $rg_name -Location $location -Tag @{Billto=$pe;ProjectName=$projectName;ProjectOwner=$projectOwner}
+            $created = $true
         }
-    } else {
-    if ($add)  { 
-            Write-Verbose "$ResourceGroupName exists in location -Add is set to $true" 
-            return $true
-        } else {
-            Write-Verbose "$ResourceGroupName exists in location -Add is not set" 
-            return $false 
+        else {
+            Write-Host -ForegroundColor Yellow "The RG $rg_name exists"
+            $num+=1
         }
-    } 
-}
+    }
+    #sets parameters for the Policy
+    $param = @{"tagName"="billto";"tagValue"=$pe.ToString()}
 
-try {
-    $sub = Get-AzureRmSubscription -SubscriptionName $subscription -ErrorAction Stop   
-    Set-AzureRmContext -Subscription $sub -ErrorAction Stop
-}
-catch {
-    Write-Host "No Such Subscription found" -ForegroundColor Yellow
-    exit
-}
+    #Gets the Enforce Tag Builtin Policy
+    $policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/1e30110a-5ceb-460c-a204-c1c3969c6d62 
 
-#Check if resource exists
-Check-ResourceGroup -ResourceGroupName $rg_name -Location $location -Verbose
+    #Applies the policy to the resource group
+    New-AzureRmPolicyAssignment -Name BilltoEnforce -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
 
-
-
-
-
-Get-AzureRmResourceGroup -Name $rg_name -ev notPresent -ea 0 
-
-if ($notPresent)
-{
-    # ResourceGroup doesn't exist
-}
-else
-{
-    # ResourceGroup exist
+    #Geta the Apply Tag Builtin Policy
+    $policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/2a0e14a6-b0a6-4fab-991a-187a4f81c498
+    #Applies the policy to the resource group
+    New-AzureRmPolicyAssignment -Name BilltoEnforce -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
 }
 
 
 
-#Creates the resource group in the Canadian Data Centre
-$rg = New-AzureRmResourceGroup -Name $rg_name -Location $location -Tag @{Billto=$pe;ProjectName=$projectName;ProjectOwner=$projectOwner}
+
 
 #adds group to the contributor role within the Resource Group
-New-AzureRmRoleAssignment -ObjectId $ad_objid.Id -ResourceGroupName $rg.ResourceGroupName -RoleDefinitionName Contributor
-
-#Gets the Enforce Tag Builtin Poicy
-$policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/1e30110a-5ceb-460c-a204-c1c3969c6d62 
+New-AzureRmRoleAssignment -ObjectId $ad_objid.Id -ResourceGroupName $rg.ResourceGroupName -RoleDefinitionName Owner
 
 #sets parameters for the Policy
 $param = @{"tagName"="billto";"tagValue"=$pe.ToString()}
 
+#Gets the Enforce Tag Builtin Policy
+$policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/1e30110a-5ceb-460c-a204-c1c3969c6d62 
+
 #Applies the policy to the resource group
 New-AzureRmPolicyAssignment -Name BilltoEnforce -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
+
+#Geta the Apply Tag Builtin Policy
+$policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/2a0e14a6-b0a6-4fab-991a-187a4f81c498
+#Applies the policy to the resource group
+New-AzureRmPolicyAssignment -Name BilltoEnforce -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
+ 
+
